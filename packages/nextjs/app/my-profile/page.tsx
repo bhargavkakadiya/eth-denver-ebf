89e4d400553f10a525a9a587bbdc1b34f33ed0f4
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2024-02-29 16:30:43 -0700

    add user profile page

---
+++ packages/nextjs/app/my-profile/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+
+const Home: NextPage = () => {
+  const { address, isConnected } = useAccount();
+  const { data: userData } = useScaffoldContractRead({
+    contractName: "EBF",
+    functionName: "users",
+    args: [address],
+  });
+
+  return (
+    <div className="flex items-center flex-col flex-grow pt-10">
+      {isConnected && userData && (
+        <div className="bg-gray-200 p-4">
+          <h1 className="text-2xl font-bold">Player Info</h1>
+          <div className="flex space-x-4">
+            <div className="mb-1">
+              <div className="mb-1">
+                <div className="mb-2">
+                  <h4 className="mb-1">
+                    <strong>Name:</strong> {String(userData[1])}
+                  </h4>
+                </div>
+                <div className="mb-2">
+                  <h4 className="mb-1">
+                    <strong>Hometown:</strong> {String(userData[2])}
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
