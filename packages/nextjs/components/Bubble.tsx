9837077dda2ff4942d1bb151d60ad6ca96101b62
Dharmik79 <ddholariya199@gmail.com>
2024-02-29 18:01:41 -0700

    removed the unnecessary code for the bubble

--- packages/nextjs/components/Bubble.tsx
+++ packages/nextjs/components/Bubble.tsx
@@ -10,6 +10,15 @@ import "react-bubble-ui/dist/index.css";
 /* eslint-disable prettier/prettier */
 // myComponent.js
 
+/* eslint-disable prettier/prettier */
+// myComponent.js
+
+/* eslint-disable prettier/prettier */
+// myComponent.js
+
+/* eslint-disable prettier/prettier */
+// myComponent.js
+
 const ChildComponent = ({ name }: { name: any }) => {
   const smallerCircleSize = 80;
   const mainCircleDiameter = 320;
@@ -17,29 +26,32 @@ const ChildComponent = ({ name }: { name: any }) => {
   const smallerCircleRadius = smallerCircleSize / 2;
 
   const positioningRadius = mainCircleRadius - smallerCircleRadius - 20;
+
   return (
-    <div className="relative bg-primary rounded-full w-80 h-80 flex items-center justify-center transition-transform duration-300 ease-in-out hover:scale-110">
-      <p className="text-white text-sm z-10">{name}</p>
-      {Array.from({ length: 6 }).map((_, index) => {
-        const angle = (Math.PI / 3) * index;
-        const position = {
-          left: `calc(50% + ${positioningRadius * Math.cos(angle)}px)`,
-          top: `calc(50% + ${positioningRadius * Math.sin(angle)}px)`,
-        };
-        return (
-          <div
-            key={index}
-            className="absolute bg-secondary rounded-full"
-            style={{
-              ...position,
-              width: `${smallerCircleSize}px`,
-              height: `${smallerCircleSize}px`,
-              transform: "translate(-50%, -50%)",
-            }}
-          ></div>
-        );
-      })}
-    </div>
+    <>
+      <div className="relative bg-primary rounded-full w-80 h-80 flex items-center justify-center ">
+        <p className="text-white text-sm z-10">{name}</p>
+        {Array.from({ length: 6 }).map((_, index) => {
+          const angle = (Math.PI / 3) * index;
+          const position = {
+            left: `calc(50% + ${positioningRadius * Math.cos(angle)}px)`,
+            top: `calc(50% + ${positioningRadius * Math.sin(angle)}px)`,
+          };
+          return (
+            <div
+              key={index}
+              className="absolute bg-secondary rounded-full"
+              style={{
+                ...position,
+                width: `${smallerCircleSize}px`,
+                height: `${smallerCircleSize}px`,
+                transform: "translate(-50%, -50%)",
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default function Bubble() {
