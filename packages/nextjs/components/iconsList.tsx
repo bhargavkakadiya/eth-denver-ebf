c2968487452dfef07beac619347ab7e6b66c1d6f
Dharmik79 <ddholariya199@gmail.com>
2024-03-01 13:47:52 -0700

    added the list of the icons

---
+++ packages/nextjs/components/iconsList.tsx
@@ -0,0 +1,35 @@
+import Air from "../components/Icons/Air";
+import Carbon from "../components/Icons/Carbon";
+import Diversity from "../components/Icons/Diversity";
+import Equity from "../components/Icons/Equity";
+import Soil from "../components/Icons/Soil";
+import Water from "../components/Icons/Water";
+
+const list = [
+  {
+    name: "Air",
+    icon: Air({ width: 50, height: 50 }),
+  },
+  {
+    name: "Soil",
+    icon: Soil({ width: 50, height: 50 }),
+  },
+  {
+    name: "Diversity",
+    icon: Diversity({ width: 50, height: 50 }),
+  },
+  {
+    name: "Water",
+    icon: Water({ width: 50, height: 50 }),
+  },
+  {
+    name: "Equity",
+    icon: Equity({ width: 50, height: 50 }),
+  },
+  {
+    name: "Carbon",
+    icon: Carbon({ width: 50, height: 50 }),
+  },
+];
+
+export default list;
