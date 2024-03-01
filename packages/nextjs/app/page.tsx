f0299dd51f9fc320692e5780559ffe9e41230718
Dharmik79 <ddholariya199@gmail.com>
2024-02-29 21:45:00 -0700

    updated the lint error

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 
+import React from "react";
 import Bubble from "../components/Bubble";
-import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <>
-      <div className="flex items-center flex-col flex-grow pt-10 w-full h-full">
-        <Bubble />
-      </div>
-    </>
+    <div className="flex items-center flex-col flex-grow pt-10 w-full h-full">
+      <Bubble />
+    </div>
   );
 };
 
--- packages/nextjs/components/tooltip/CloseIcon.tsx
+++ packages/nextjs/components/tooltip/CloseIcon.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import CloseIcon from '@mui/icons-material/Close';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
+import * as React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
 
-export default function BasicTooltip({onClose}:{onClose: any}) {
+export default function BasicTooltip({ onClose }: { onClose: any }) {
   return (
-    <Tooltip title="Delete" >
+    <Tooltip title="Delete">
       <IconButton>
-        <CloseIcon onClick={onClose}/>
+        <CloseIcon onClick={onClose} />
       </IconButton>
     </Tooltip>
   );
